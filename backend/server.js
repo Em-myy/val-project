023b5c53fd91f70d68db0412b7949fffc4cdbacd
Famuyiwa Emmanuel <famuyiwaemmanuel565@gmail.com>
2026-02-11 12:09:01 +0100

    fixed cors issue

--- backend/server.js
+++ backend/server.js
@@ -7,7 +7,11 @@ import cors from "cors";
 
 const app = express();
 app.use(express.json());
-app.use(cors());
+app.use(
+  cors({
+    origin: process.env.API_URL,
+  }),
+);
 
 const transporter = nodemailer.createTransport({
   service: "gmail",
